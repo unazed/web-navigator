96e3b51c9c8edfca2825319b65c5935673cb48a1
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-12-09 00:47:32 +0000

    Add files via upload

---
+++ main.d
@@ -0,0 +1,46 @@
+import std.socket;
+import std.stdio;
+import std.conv;
+import std.format;
+import std.algorithm;
+import std.string;
+import std.array;
+import core.exception;
+
+string getUrlData(string url, ushort port, int receive_size = 1024, string header = null)
+{
+	Socket s = new TcpSocket();
+	Address addr = getAddress(url, port)[0];
+	
+	s.connect(addr);
+
+	char[] buffer;
+	buffer.length = receive_size;
+
+	s.send(header ? header : format!"GET / HTTP/1.1\r\nHost: %s\r\n\r\n"(url));
+	s.receive(buffer);
+	s.close();
+
+	return to!string(buffer);
+}
+
+void main()
+{
+	while (1)
+	{
+		write("Enter website, port and receive segment size: ");
+		auto line = readln.chomp;
+		auto res = splitter(line, ' ').array;
+
+		writeln(res);
+
+		try {
+			writeln(getUrlData(res[0], to!ushort(res[1]), to!int(res[2])));
+		} catch (RangeError)
+		{ writeln("Enter the correct amount of parameters please.");
+		} catch (ConvException)
+		{ writeln("Please enter correct datatypes."); 
+		} catch (SocketOSException)
+		{ writeln("Enter a valid hostname/port."); }
+	}
+}
